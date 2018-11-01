91efd2850a2a6d36a2271e7258e0d463a306ea88
Patrick Shriwise <pshriwise@gmail.com>
2018-11-01 12:46:38 -0500

    Extending ray fire program a bit.

--- apps/moab_rf.cpp
+++ apps/moab_rf.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <cassert>
 
 #include "moab/ProgOptions.hpp"
 #include "RTI.hpp"
@@ -19,26 +20,26 @@ int main(int argc, char** argv) {
   moab::ErrorCode rval;
   rval = RTI->init(filename);
   MB_CHK_SET_ERR(rval, "Failed to initialize the RayTracingInterface.");
-  
-  RTCDRay ray;
 
-  double org[3] = {0.0, 0.0, 0.0};
-  ray.set_org(org);
-  double dir[3] = {0.5, 0.5, 0.5};
-  ray.set_dir(dir);
-  double len = 1e17;
-  ray.set_len(len);
-  
   moab::Range vols;
   rval = RTI->get_vols(vols);
   MB_CHK_SET_ERR(rval, "Failed to get volumes from the RTI.");
-
-  RTI->fire(vols.front(), ray);
-
-  std::cout << "Ray Distance: " << ray.dtfar << std::endl;
+  
+  for(int i = 0; i < 10000000 ; i++) {
+    // setup ray
+    RTCDRay ray;
+    double org[3] = {0.0, 0.0, 0.0};
+    ray.set_org(org);
+    moab::CartVect dir = {1.0, 1.0, 1.0};
+    dir.normalize(); // unit len distance always
+    ray.set_dir(dir.array());
+    double len = 1e17;
+    ray.set_len(len);
+  
+    // fire ray
+    RTI->fire(vols.front(), ray);
+  }
   
   delete RTI;
   return 0;
 }
-
-
