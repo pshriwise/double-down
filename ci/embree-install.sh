0dd14a72a79bca6f58c0bc03751455660ffe0ffc
Patrick Shriwise <pshriwise@gmail.com>
2018-11-01 11:12:11 -0500

    Corrections to embree install script.

--- ci/embree-install.sh
+++ ci/embree-install.sh
@@ -9,14 +9,13 @@ EMBREE_INSTALL_DIR=$HOME/EMBREE/
 CURRENT_DIR=$(pwd)
 
 # Embree Install
-# MOAB Install
 cd $HOME
 mkdir EMBREE && cd EMBREE
 git clone -b $EMBREE_TAG $EMBREE_REPO
 mkdir build && cd build
-cmake ../moab -DCMAKE_INSTALL_PREFIX=$EMBREE_INSTALL_DIR \
+cmake ../embree -DCMAKE_INSTALL_PREFIX=$EMBREE_INSTALL_DIR \
       -DEMBREE_ISPC_SUPPORT=OFF \
       -DEMBREE_TBB_ROOT=/usr
 make -j && make -j test install
-rm -rf $HOME/EMBREE/moab
+rm -rf $HOME/EMBREE/embree
 export LD_LIBRARY_PATH=$EMBREE_INSTALL_DIR/lib:$LD_LIBRARY_PATH
