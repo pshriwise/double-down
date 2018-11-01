976f351c61b212010fa5fc98efbb37809617f8b9
Patrick Shriwise <pshriwise@gmail.com>
2018-11-01 12:58:51 -0500

    Updating rf program output.

--- apps/moab_rf.cpp
+++ apps/moab_rf.cpp
@@ -1,10 +1,24 @@
 #include <string>
 #include <iostream>
 #include <cassert>
+#include <ctime>
 
 #include "moab/ProgOptions.hpp"
 #include "RTI.hpp"
 #include "ray.h"
+
+static const double PI = acos(-1.0);
+static const double denom = 1.0 / ((double) RAND_MAX);
+static const double denomPI = PI * denom;
+
+inline void RNDVEC(moab::CartVect& uvw) {
+  // denom normalizes rand values (see global defines)
+  double theta = 2 * denomPI * rand(); // randomly samples from 0 to az. (Default az is 2PI)
+  double u = 2 * denom * rand() - 1; // randomly samples from -1 to 1.
+  uvw[0] = sqrt(1 - u * u) * cos(theta);
+  uvw[1] = sqrt(1 - u * u) * sin(theta);
+  uvw[2] = u;
+}
 
 int main(int argc, char** argv) {
 
@@ -24,22 +38,37 @@ int main(int argc, char** argv) {
   moab::Range vols;
   rval = RTI->get_vols(vols);
   MB_CHK_SET_ERR(rval, "Failed to get volumes from the RTI.");
-  
-  for(int i = 0; i < 10000000 ; i++) {
+
+  int num_rays = 1000000;
+  double total= 0.0;
+  std::clock_t mark;
+  for (int i = 0; i < num_rays; i++) {
     // setup ray
     RTCDRay ray;
     double org[3] = {0.0, 0.0, 0.0};
     ray.set_org(org);
-    moab::CartVect dir = {1.0, 1.0, 1.0};
+    moab::CartVect dir;
+    RNDVEC(dir);
     dir.normalize(); // unit len distance always
     ray.set_dir(dir.array());
     double len = 1e17;
     ray.set_len(len);
-  
+
+    // make sure we hit something
+    assert(ray.geomID != -1);
+    
     // fire ray
+    mark = std::clock();
     RTI->fire(vols.front(), ray);
+    total += std::clock() - mark;
   }
+
+  double total_sec = total / (double)CLOCKS_PER_SEC;
+  double per_ray = total_sec / (double)num_rays;
   
+  std::cout << "Total time in Ray Fire: " << total_sec << " sec" << std::endl;
+  std::cout << "Total time per Ray Fire: " << per_ray << " sec" << std::endl;
+
   delete RTI;
   return 0;
 }
