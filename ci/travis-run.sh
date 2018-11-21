7b76af9fea44a786257ba419840fff9dd4e318b4
Patrick Shriwise <pshriwise@gmail.com>
2018-11-20 21:44:31 -0600

    Updating install location on CI.

--- ci/travis-run.sh
+++ ci/travis-run.sh
@@ -4,6 +4,6 @@ set -ex
 mkdir $HOME/opt/ && cd $HOME/opt/
 git clone https://github.com/pshriwise/double-down $DDHOME
 mkdir $DDBUILD && cd $DDBUILD
-cmake $DDHOME
+cmake $DDHOME -DCMAKE_INSTALL_PREFIX=$HOME/dd/
 make && make test
 
