b7caccafeb5ec003738d4fa6feb22aa02bb4520f
Patrick Shriwise <pshriwise@gmail.com>
2018-11-27 16:12:05 -0600

    Updating MOAB branch to use for CI temporarily.

--- ci/moab-install.sh
+++ ci/moab-install.sh
@@ -2,8 +2,8 @@
 set -ex
 
 # MOAB Variables
-MOAB_BRANCH='Version5.0'
-MOAB_REPO='https://bitbucket.org/fathomteam/moab/'
+MOAB_BRANCH='rh_separate'
+MOAB_REPO='https://bitbucket.org/pshriwise/moab/'
 MOAB_INSTALL_DIR=$HOME/MOAB/
 
 CURRENT_DIR=$(pwd)
