1e54ce15d6f7a5429353110dce489631be06d9bf
Patrick Shriwise <pshriwise@gmail.com>
2019-01-30 22:24:55 -0600

    Factoring out bounds function.

--- src/primitives.cpp
+++ src/primitives.cpp
@@ -1,9 +1,9 @@
 
 #include "primitives.hpp"
 
-void intersectionFilter(void* ptr, RTCDRay &ray) 
+void intersectionFilter(void* ptr, RTCDRay &ray)
 {
-  switch(ray.rf_type) 
+  switch(ray.rf_type)
     {
     case 0: //if this is a typical ray_fire, check the dot_product
       if ( 0 > ray.dot_prod() )
@@ -18,39 +18,11 @@ void intersectionFilter(void* ptr, RTCDRay &ray)
 void DblTriBounds(void* tris_i, size_t item, RTCBounds& bounds_o) {
 
   const DblTri* tris = (const DblTri*) tris_i;
-  const DblTri& this_tri = tris[item];  
+  const DblTri& this_tri = tris[item];
 
   moab::Interface* mbi = (moab::Interface*) this_tri.moab_instance;
-  moab::ErrorCode rval;
 
-  std::vector<moab::EntityHandle> conn;
-  rval = mbi->get_connectivity(&(this_tri.handle), 1, conn);
-  MB_CHK_SET_ERR_CONT(rval, "Failed to get triangle connectivity");
-
-  if (conn.size() != 3) {
-    throw std::length_error("Incorrect number of coordinates returned for a triangle entity.");
-  }
-  
-  moab::CartVect coords[3];
-  rval = mbi->get_coords(&conn[0], 1, coords[0].array());
-  MB_CHK_SET_ERR_CONT(rval, "Failed to get vertext coordinates");
-  rval = mbi->get_coords(&conn[1], 1, coords[1].array());
-  MB_CHK_SET_ERR_CONT(rval, "Failed to get vertext coordinates");
-  rval = mbi->get_coords(&conn[2], 1, coords[2].array());
-  MB_CHK_SET_ERR_CONT(rval, "Failed to get vertext coordinates");
-
-  double bump_val = 5e-03;
-  
-  bounds_o.lower_x = std::min(coords[0][0],std::min(coords[1][0],coords[2][0]));
-  bounds_o.lower_y = std::min(coords[0][1],std::min(coords[1][1],coords[2][1]));
-  bounds_o.lower_z = std::min(coords[0][2],std::min(coords[1][2],coords[2][2]));
-
-  bounds_o.upper_x = std::max(coords[0][0],std::max(coords[1][0],coords[2][0]));
-  bounds_o.upper_y = std::max(coords[0][1],std::max(coords[1][1],coords[2][1]));
-  bounds_o.upper_z = std::max(coords[0][2],std::max(coords[1][2],coords[2][2]));
-  
-  bounds_o.lower_x -= bump_val; bounds_o.lower_y -= bump_val; bounds_o.lower_z -= bump_val; 
-  bounds_o.upper_x += bump_val; bounds_o.upper_y += bump_val; bounds_o.upper_z += bump_val; 
+  bounds_o = DblTriBounds(mbi, this_tri.handle);
 
   return;
 }
@@ -58,8 +30,8 @@ void DblTriBounds(void* tris_i, size_t item, RTCBounds& bounds_o) {
 void DblTriIntersectFunc(void* tris_i, RTCDRay& ray, size_t item) {
 
   const DblTri* tris = (const DblTri*) tris_i;
-  const DblTri& this_tri = tris[item];  
-  
+  const DblTri& this_tri = tris[item];
+
   moab::Interface* mbi = (moab::Interface*) this_tri.moab_instance;
   moab::ErrorCode rval;
 
@@ -74,7 +46,7 @@ void DblTriIntersectFunc(void* tris_i, RTCDRay& ray, size_t item) {
   MB_CHK_SET_ERR_CONT(rval, "Failed to get vertext coordinates");
   rval = mbi->get_coords(&conn[2], 1, &(coords[2][0]));
   MB_CHK_SET_ERR_CONT(rval, "Failed to get vertext coordinates");
-  
+
   double dist;
   double nonneg_ray_len = 1e17;
   const double* ptr = &nonneg_ray_len;
@@ -82,17 +54,17 @@ void DblTriIntersectFunc(void* tris_i, RTCDRay& ray, size_t item) {
   Vec3da ray_dir(ray.ddir);
 
   bool hit = plucker_ray_tri_intersect(coords, ray_org, ray_dir, dist, ptr);
-  
+
   if ( hit ) {
     if (ray.geomID != -1 && dist > ray.dtfar) { return; }
-    
+
     ray.dtfar = dist;
     ray.tfar = dist;
     ray.u = 0.0f;
     ray.v = 0.0f;
     ray.geomID = this_tri.geomID;
     ray.primID = (unsigned int) item;
-    
+
     Vec3da normal = cross((coords[1] - coords[0]),(coords[2] - coords[0]));
 
     if( -1 == this_tri.sense ) normal *= -1;
@@ -110,7 +82,7 @@ void DblTriIntersectFunc(void* tris_i, RTCDRay& ray, size_t item) {
 void DblTriOccludedFunc(void* tris_i, RTCDRay& ray, size_t item) {
 
   const DblTri* tris = (const DblTri*) tris_i;
-  const DblTri& this_tri = tris[item];  
+  const DblTri& this_tri = tris[item];
   RTCBounds bounds;
 
   moab::Interface* mbi = (moab::Interface*) this_tri.moab_instance;
@@ -129,7 +101,7 @@ void DblTriOccludedFunc(void* tris_i, RTCDRay& ray, size_t item) {
   double* ptr = &nonneg_ray_len;
   Vec3da ray_org(ray.dorg);
   Vec3da ray_dir(ray.ddir);
-  
+
   bool hit = plucker_ray_tri_intersect(coords, ray_org, ray_dir, dist, ptr);
   if ( hit ) {
     ray.geomID = 0;
--- src/primitives.hpp
+++ src/primitives.hpp
@@ -17,6 +17,43 @@ struct DblTri {
   int sense;
 };
 
+inline RTCBounds DblTriBounds(const moab::Interface *mbi, moab::EntityHandle tri_handle) {
+  moab::ErrorCode rval;
+
+  std::vector<moab::EntityHandle> conn;
+  rval = mbi->get_connectivity(&tri_handle, 1, conn);
+  MB_CHK_SET_ERR_CONT(rval, "Failed to get triangle connectivity");
+
+  if (conn.size() != 3) {
+    throw std::length_error("Incorrect number of coordinates returned for a triangle entity.");
+  }
+
+  moab::CartVect coords[3];
+  rval = mbi->get_coords(&conn[0], 1, coords[0].array());
+  MB_CHK_SET_ERR_CONT(rval, "Failed to get vertext coordinates");
+  rval = mbi->get_coords(&conn[1], 1, coords[1].array());
+  MB_CHK_SET_ERR_CONT(rval, "Failed to get vertext coordinates");
+  rval = mbi->get_coords(&conn[2], 1, coords[2].array());
+  MB_CHK_SET_ERR_CONT(rval, "Failed to get vertext coordinates");
+
+  double bump_val = 5e-03;
+
+  RTCBounds bounds_o;
+  bounds_o.lower_x = std::min(coords[0][0],std::min(coords[1][0],coords[2][0]));
+  bounds_o.lower_y = std::min(coords[0][1],std::min(coords[1][1],coords[2][1]));
+  bounds_o.lower_z = std::min(coords[0][2],std::min(coords[1][2],coords[2][2]));
+
+  bounds_o.upper_x = std::max(coords[0][0],std::max(coords[1][0],coords[2][0]));
+  bounds_o.upper_y = std::max(coords[0][1],std::max(coords[1][1],coords[2][1]));
+  bounds_o.upper_z = std::max(coords[0][2],std::max(coords[1][2],coords[2][2]));
+
+  bounds_o.lower_x -= bump_val; bounds_o.lower_y -= bump_val; bounds_o.lower_z -= bump_val;
+  bounds_o.upper_x += bump_val; bounds_o.upper_y += bump_val; bounds_o.upper_z += bump_val;
+
+  return bounds_o;
+
+}
+
 void DblTriBounds(void* tris_i, size_t item, RTCBounds& bounds_o);
 
 void DblTriIntersectFunc(void* tris_i, RTCDRay& ray, size_t item);
