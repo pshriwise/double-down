6c0037496499c499dec898ceefc7e3a6385ae4d1
Patrick Shriwise <pshriwise@gmail.com>
2019-07-01 09:32:45 -0500

    Adding a simple ray fire test.

--- test/test_rf.cpp
+++ test/test_rf.cpp
@@ -16,11 +16,28 @@ int main() {
   rval = MBI->load_file("../test_files/sphere.h5m");
   MB_CHK_SET_ERR(rval, "Failed to load test file");
 
-  RayTracingInterface* RTI = new RayTracingInterface(MBI);
+  std::unique_ptr<RayTracingInterface> RTI(new RayTracingInterface(MBI));
 
   rval = RTI->init();
   MB_CHK_SET_ERR(rval, "Failed to initialize the RTI.");
 
+  moab::Range vols;
+  rval = RTI->get_vols(vols);
+  MB_CHK_SET_ERR(rval, "Failed to get volumes from the RTI.");
+
+  moab::EntityHandle sphere_vol = vols[0];
+
+  // fire a test ray
+  double org[3] = {0.0, 0.0, 0.0};
+  double dir[3] = {1.0, 0.0, 0.0};
+
+  double dist = 0.0;
+  moab::EntityHandle surf;
+
+  RTI->dag_ray_fire(sphere_vol, org, dir, surf, dist);
+
+  if (dist == 0.0) { return 1; }
+  if (surf == 0) { return 1; }
 
   return 0;
 }
