f4cc839a3a3c9e227dc4ff9b18223ecaed873409
Patrick Shriwise <pshriwise@gmail.com>
2019-07-04 13:48:32 -0500

    Removing redundant (and incorrect) git chckout from travis file.

--- ci/travis-run.sh
+++ ci/travis-run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -ex
 
-mkdir $HOME/opt/ && cd $HOME/opt/
-git clone https://github.com/pshriwise/double-down $DDHOME
-mkdir $DDBUILD && cd $DDBUILD
-cmake $DDHOME -DCMAKE_INSTALL_PREFIX=$HOME/dd/
+mkdir build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/dd/
 make && make test && make install
