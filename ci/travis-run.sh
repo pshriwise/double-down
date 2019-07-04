072b453944fd477c9a8c6a5a4dc238907afcb230
Patrick Shriwise <pshriwise@gmail.com>
2019-07-04 10:53:19 -0500

    Adding DD install so DAGMC can find it.

--- ci/travis-run.sh
+++ ci/travis-run.sh
@@ -5,5 +5,4 @@ mkdir $HOME/opt/ && cd $HOME/opt/
 git clone https://github.com/pshriwise/double-down $DDHOME
 mkdir $DDBUILD && cd $DDBUILD
 cmake $DDHOME -DCMAKE_INSTALL_PREFIX=$HOME/dd/
-make && make test
-
+make && make test && make install
