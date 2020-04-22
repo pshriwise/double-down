9025f3bdf07c7ec9863cb50a43ef252f4b955d3b
Patrick Shriwise <pshriwise@gmail.com>
2020-04-22 02:24:17 -0500

    Updating CI CMake commands.

--- ci/dagmc-install.sh
+++ ci/dagmc-install.sh
@@ -8,14 +8,16 @@ DAGMC_INSTALL_DIR=$HOME/DAGMC/
 
 CURRENT_DIR=$(pwd)
 
-export LD_LIBRARY_PATH=$HOME/dd/lib:$LD_LIBRARY_PATH
-
 # DAGMC Install
 cd $HOME
 mkdir DAGMC && cd DAGMC
 git clone -b $DAGMC_BRANCH $DAGMC_REPO
 mkdir build && cd build
-cmake ../dagmc -DMOAB_DIR=$HOME/MOAB/ -DCMAKE_INSTALL_PREFIX=$HOME/DAGMC/ -DDOUBLE_DOWN=ON -DBUILD_STATIC_LIBS=OFF
+cmake ../dagmc -DCMAKE_INSTALL_PREFIX=$HOME/DAGMC/ \
+      -DMOAB_DIR=$HOME/MOAB/ \
+      -DDOUBLE_DOWN=ON \
+      -DDOUBLE_DOWN_DIR=$HOME/dd \
+      -DBUILD_STATIC_LIBS=OFF
 make -j 3 install
 make -j 3 ; cd ./src/dagmc/tests/; ./dagmc_simple_test
 make -j 3 test
--- ci/travis-run.sh
+++ ci/travis-run.sh
@@ -2,5 +2,7 @@
 set -ex
 
 mkdir build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/dd/
+cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/dd/ \
+      -DMOAB_DIR=$HOME/MOAB \
+      -DEMBREE_DIR=$HOME/EMBREE
 make && make test && make install
