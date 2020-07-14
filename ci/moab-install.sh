8251cfcf74852a33c321c46f8249b2439494a5fe
Patrick Shriwise <pshriwise@gmail.com>
2020-07-14 14:09:31 -0500

    Disabling blas/lapack in moad build file for CI.

--- ci/moab-install.sh
+++ ci/moab-install.sh
@@ -13,7 +13,7 @@ cd $HOME
 mkdir MOAB && cd MOAB
 git clone -b $MOAB_BRANCH $MOAB_REPO
 mkdir build && cd build
-cmake ../moab -DENABLE_HDF5=ON -DCMAKE_INSTALL_PREFIX=$MOAB_INSTALL_DIR
+cmake ../moab -DENABLE_HDF5=ON -DCMAKE_INSTALL_PREFIX=$MOAB_INSTALL_DIR -DENABLE_BLASLAPACK=OFF
 make -j 3 && make -j 3 test install
 cmake ../moab -DBUILD_SHARED_LIBS=OFF
 make -j 3 install
