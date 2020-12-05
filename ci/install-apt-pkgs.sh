aa6358bd127625befe7dee1a0d4e9e1f7e724676
Patrick Shriwise <pshriwise@gmail.com>
2020-12-05 17:42:26 -0600

    Adding script for installing aptitude packages in CI.

---
+++ ci/install-apt-pkgs.sh
@@ -0,0 +1,13 @@
+set -ex
+
+apt install -y \
+    gfortran \
+    mpich \
+    libmpich-dev \
+    libhdf5-serial-dev \
+    libhdf5-mpich-dev \
+    libeigen3-dev \
+    libtbb-dev \
+    freeglut3-dev \
+    libglfw3 \
+    libglfw3-dev
