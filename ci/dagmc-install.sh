4515bf71dcdc2cd2a41db3d2f5bc605b5797bf07
Patrick Shriwise <pshriwise@gmail.com>
2020-12-05 19:36:41 -0600

    Updating CMake variable used to find double-down in CI.

--- ci/dagmc-install.sh
+++ ci/dagmc-install.sh
@@ -16,7 +16,7 @@ mkdir build && cd build
 cmake ../dagmc -DCMAKE_INSTALL_PREFIX=$HOME/DAGMC/ \
       -DMOAB_DIR=$HOME/MOAB/ \
       -DDOUBLE_DOWN=ON \
-      -DDOUBLE_DOWN_DIR=$HOME/dd \
+      -DCMAKE_PREFIX_PATH=$HOME/dd \
       -DBUILD_STATIC_LIBS=OFF
 make -j 3 install
 make -j 3 ; cd ./src/dagmc/tests/; ./dagmc_simple_test
