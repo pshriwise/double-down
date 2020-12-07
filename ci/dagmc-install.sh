9b267ed8b488aa028ec1be118a2d3822bbe1b009
Patrick Shriwise <pshriwise@gmail.com>
2020-12-06 21:37:08 -0600

    Adding double down directory.

--- ci/dagmc-install.sh
+++ ci/dagmc-install.sh
@@ -17,6 +17,7 @@ cmake ../dagmc -DCMAKE_INSTALL_PREFIX=$HOME/DAGMC/ \
       -DMOAB_DIR=$HOME/MOAB/ \
       -DDOUBLE_DOWN=ON \
       -DCMAKE_PREFIX_PATH=$HOME/dd/lib \
+      -DDOUBLE_DOWN_DIR=$HOME/dd \
       -DBUILD_STATIC_LIBS=OFF
 make -j 3 install
 make -j 3 ; cd ./src/dagmc/tests/; ./dagmc_simple_test
