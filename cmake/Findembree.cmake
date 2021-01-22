cf01c4f019b52f3ce1015fc4556cf9e05e0b9c9c
Patrick Shriwise <pshriwise@gmail.com>
2021-01-22 15:01:34 -0600

    Updating Findembree.cmake to set Embree version automaticall based on the version header.

--- cmake/Findembree.cmake
+++ cmake/Findembree.cmake
@@ -9,16 +9,55 @@
 #  EMBREE_LIBRARIES - Link these to use MOAB
 #  EMBREE_DEFINITIONS - Compiler switches required for using MOAB
 
-set(EMBREE_VERSION ${embree_FIND_VERSION_MAJOR}.${embree_FIND_VERSION_MINOR}.${embree_FIND_VERSION_PATCH})
+find_path(EMBREE_INCLUDE_DIR embree3/rtcore.h
+  HINTS
+  "${EMBREE_DIR}/include"
+  "$ENV{EMBREE_ROOT}/include"
+  "${EMBREE_DIR}/../include"
+  "${EMBREE_DIR}/../../include"
+  DOC "Embree headers path")
+
+message(STATUS "Embree include dir: ${EMBREE_INCLUDE_DIR}")
+
+if (EMBREE_INCLUDE_DIR AND EXISTS "${EMBREE_INCLUDE_DIR}/embree3/rtcore_version.h" )
+    file(STRINGS "${EMBREE_INCLUDE_DIR}/embree3/rtcore_version.h" TMP REGEX "^#define RTC_VERSION_MAJOR.*$")
+    string(REGEX MATCHALL "[0-9]+" MAJOR ${TMP})
+    file(STRINGS "${EMBREE_INCLUDE_DIR}/embree3/rtcore_version.h" TMP REGEX "^#define RTC_VERSION_MINOR.*$")
+    string(REGEX MATCHALL "[0-9]+" MINOR ${TMP})
+    file(STRINGS "${EMBREE_INCLUDE_DIR}/embree3/rtcore_version.h" TMP REGEX "^#define RTC_VERSION_PATCH.*$")
+    string(REGEX MATCHALL "[0-9]+" PATCH ${TMP})
+
+    set (EMBREE_VERSION ${MAJOR}.${MINOR}.${PATCH})
+endif()
+
+if (NOT DEFINED EMBREE_VERSION AND EXISTS "${EMBREE_INCLUDE_DIR}/embree3/rtcore_config.h" )
+    file(STRINGS "${EMBREE_INCLUDE_DIR}/embree3/rtcore_config.h" TMP REGEX "^#define RTC_VERSION_MAJOR.*$")
+    string(REGEX MATCHALL "[0-9]+" MAJOR ${TMP})
+    file(STRINGS "${EMBREE_INCLUDE_DIR}/embree3/rtcore_config.h" TMP REGEX "^#define RTC_VERSION_MINOR.*$")
+    string(REGEX MATCHALL "[0-9]+" MINOR ${TMP})
+    file(STRINGS "${EMBREE_INCLUDE_DIR}/embree3/rtcore_config.h" TMP REGEX "^#define RTC_VERSION_PATCH.*$")
+    string(REGEX MATCHALL "[0-9]+" PATCH ${TMP})
+
+    set (EMBREE_VERSION ${MAJOR}.${MINOR}.${PATCH})
+endif()
+
+if (NOT DEFINED EMBREE_VERSION OR "${EMBREE_VERSION}" STREQUAL "..")
+  message(FATAL_ERROR "Could not find Embree 3 in the install location: ${EMBREE_DIR}")
+endif()
 
 message(STATUS "Searching for embree ${EMBREE_VERSION}...")
 
 find_path(EMBREE_CMAKE_CONFIG NAMES embree-config.cmake
-          HINTS ${EMBREE_ROOT}
-          PATHS ENV LD_LIBRARY_PATH
-          PATHS ${EMBREE_DIR}
-          PATH_SUFFIXES lib Lib cmake cmake/embree-${EMBREE_VERSION} lib/cmake/embree-${EMBREE_VERSION}
-          NO_DEFAULT_PATH)
+  HINTS ${EMBREE_DIR}
+  PATHS ENV LD_LIBRARY_PATH
+  PATH_SUFFIXES lib Lib cmake cmake/embree-${EMBREE_VERSION} lib/cmake/embree-${EMBREE_VERSION}
+  NO_DEFAULT_PATH)
+
+if (NOT DEFINED EMBREE_CMAKE_CONFIG)
+  message(FATAL_ERROR "Could not find the Embree CMake configuration file.")
+endif()
+
+
 message(STATUS "Found EMBREE in ${EMBREE_CMAKE_CONFIG}")
 
 include(${EMBREE_CMAKE_CONFIG}/embree-config.cmake)
