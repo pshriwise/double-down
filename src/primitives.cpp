829ac03dcd035124d21e283c416ff513d868fac4
Patrick Shriwise <pshriwise@gmail.com>
2021-04-18 23:44:20 +0000

    Adding docstrings and comments to primitives.

--- src/primitives.cpp
+++ src/primitives.cpp
@@ -11,91 +11,104 @@ void intersectionFilter(void* ptr, RTCDRayHit &rayhit)
 {
   switch(rayhit.ray.rf_type)
     {
-    case 0: //if this is a typical ray_fire, check the dot_product
-      if ( 0 > rayhit.dot_prod() )
-	rayhit.hit.geomID = RTC_INVALID_GEOMETRY_ID;
+    case RayFireType::RF: //if this is a typical ray_fire, check the dot_product
+      if (0 > rayhit.dot_prod()) { rayhit.hit.geomID = RTC_INVALID_GEOMETRY_ID; }
       break;
-    case 1: //if this is a point_in_vol fire, do nothing
+    case RayFireType::PIV: //if this is a point_in_vol fire, accept all hits
       break;
     }
 }
 
-
 void DblTriBounds(const RTCBoundsFunctionArguments* args)
 {
+  // get the array of DblTri's stored on the geometry
   void* tris_i = args->geometryUserPtr;
-  size_t item = args->primID;
+  // referencde to the returned triangle bounds
   RTCBounds& bounds_o = *args->bounds_o;
 
+  // convert the void user data pointer to the DblTri pointer
   const DblTri* tris = (const DblTri*) tris_i;
-  const DblTri& this_tri = tris[item];
+  // select the DblTri in question based on the primitive ID
+  const DblTri& this_tri = tris[args->primID];
 
+  // get the MOAB direct access manager from the triangle (it's present on each one)
   MBDirectAccess* mdam = (MBDirectAccess*) this_tri.mdam;
 
+  // compute the bounding box using the direct access manager and the triangle's handle
   bounds_o = DblTriBounds(mdam, this_tri.handle);
-
-  return;
 }
 
 void DblTriIntersectFunc(RTCIntersectFunctionNArguments* args) {
-
+  // get the array of DblTri's stored on the geometry
   void* tris_i = args->geometryUserPtr;
-  size_t item = args->primID;
+  // convert the void user data pointer to the DblTri pointer
+  const DblTri* tris = (const DblTri*) tris_i;
+  // select the DblTri in question based on the primitive ID
+  const DblTri& this_tri = tris[args->primID];
+
+  // get the rayhit object from the args
   RTCDRayHit* rayhit = (RTCDRayHit*)args->rayhit;
   RTCDRay& ray = rayhit->ray;
   RTCDHit& hit = rayhit->hit;
 
-  const DblTri* tris = (const DblTri*) tris_i;
-  const DblTri& this_tri = tris[item];
-
+  // get the direct access manager from the triangle (it's present on each one)
   MBDirectAccess* mdam = (MBDirectAccess*) this_tri.mdam;
 
+  // get the triangle coordinates from the direct access manager
   std::array<moab::CartVect, 3> coords = mdam->get_mb_coords(this_tri.handle);
 
-  double dist;
-  double nonneg_ray_len = 1e17;
-  const double* ptr = &nonneg_ray_len;
+
+  double dist; // local variable for the distance to the triangle intersection
+  // convert the ray origin and directory to MOAB CartVect
   moab::CartVect ray_org(ray.dorg.x, ray.dorg.y, ray.dorg.z);
   moab::CartVect ray_dir(ray.ddir.x, ray.ddir.y, ray.ddir.z);
 
+  double nonneg_ray_len = 1e17;
+  const double* ptr = &nonneg_ray_len;
   bool hit_tri = moab::GeomUtil::plucker_ray_tri_intersect(&(coords[0]), ray_org, ray_dir, dist, ptr);
 
   if ( hit_tri ) {
+    // only accept hits that are closer than the current one
     if (dist > ray.dtfar) {
       hit.geomID = -1;
       return;
     }
-
-    ray.dtfar = dist;
-    ray.tfar = dist;
+    // set both the single and double ray distances
+    ray.set_len(dist);
+    // zero out the barycentric coordinates of the hit
     hit.u = 0.0f;
     hit.v = 0.0f;
+    // set the hit
     hit.geomID = this_tri.geomID;
-    hit.primID = (unsigned int) item;
+    hit.primID = args->primID;
 
+    // compute the triangle normal
     moab::CartVect normal = (coords[1] - coords[0]) * (coords[2] - coords[0]);
     normal.normalize();
 
+    // flip the triangle normal if the sense is reversed
     if( -1 == this_tri.sense ) normal *= -1;
 
+    // set the double precision normal of the hit
     hit.dNg[0] = normal[0];
     hit.dNg[1] = normal[1];
     hit.dNg[2] = normal[2];
   } else {
+    // otherwise set the hit's geomID as invalid to ensure we don't consider this triangle as hit
     hit.geomID = RTC_INVALID_GEOMETRY_ID;
   }
-
-  return;
 }
 
 void DblTriOccludedFunc(RTCOccludedFunctionNArguments* args) {
-
+  // get the array of DblTri's stored on the geometry
   void* tris_i = args->geometryUserPtr;
-  size_t item = args->primID;
-  RTCDRay* ray = (RTCDRay*)&(args->ray);
-
+  // convert the void user data pointer to the DblTri pointer
   const DblTri* tris = (const DblTri*) tris_i;
-  const DblTri& this_tri = tris[item];
+  // select the DblTri in question based on the primitive ID
+  const DblTri& this_tri = tris[args->primID];
+
+  // get the double precision ray from the args
+  RTCDRay* ray = (RTCDRay*)&(args->ray);
 
   MBDirectAccess* mdam = (MBDirectAccess*) mdam;
 
@@ -110,13 +123,13 @@ void DblTriOccludedFunc(RTCOccludedFunctionNArguments* args) {
   moab::CartVect ray_dir(ddir[0], ddir[1], ddir[2]);
 
   bool hit_tri = moab::GeomUtil::plucker_ray_tri_intersect(&(coords[0]), ray_org, ray_dir, dist, ptr);
+  // if we hit the triangle, we hit the triangle
   if ( hit_tri ) {
     ray->set_len(neg_inf);
   }
 }
 
 bool DblTriPointQueryFunc(RTCPointQueryFunctionArguments* args) {
-
   RTCDPointQuery* pq = (RTCDPointQuery*) args->query;
 
   double pnt[3];
@@ -124,14 +137,17 @@ bool DblTriPointQueryFunc(RTCPointQueryFunctionArguments* args) {
   pnt[1] = pq->dy;
   pnt[2] = pq->dz;
 
-  size_t item = args->primID;
-
   RTCGeometry g = rtcGetGeometry(*(RTCScene*)args->userPtr, args->geomID);
-  void* tris_i = rtcGetGeometryUserData(g);
-  const DblTri* tris = (const DblTri*) tris_i;
-  const DblTri& this_tri = tris[item];
 
+  // get the array of DblTri's stored on the geometry
+  void* tris_i = rtcGetGeometryUserData(g);
+  // convert the void user data pointer to the DblTri pointer
+  const DblTri* tris = (const DblTri*) tris_i;
+  // select the DblTri in question based on the primitive ID
+  const DblTri& this_tri = tris[args->primID];
+  // compute the distance to the nearest point on the triangle
   double dist = DblTriClosestFunc(this_tri, pnt);
+  // update the point query values if this distance is less than the current one
   if (dist < pq->dradius) {
     pq->radius = dist;
     pq->dradius = dist;
@@ -144,15 +160,15 @@ bool DblTriPointQueryFunc(RTCPointQueryFunctionArguments* args) {
 }
 
 double DblTriClosestFunc(const DblTri& tri, const double loc[3]) {
-
+  // get the direct access manager
   MBDirectAccess* mdam = (MBDirectAccess*) tri.mdam;
-
+  // get the triangle coordinates
   std::array<moab::CartVect, 3> coords = mdam->get_mb_coords(tri.handle);
-
+  // convert query location to a MOAB CartVect
   moab::CartVect location(loc[0], loc[1], loc[2]);
-
   moab::CartVect result;
-  moab::GeomUtil::closest_location_on_tri(location, &(coords[0]), result);
-
+  // compute the nearest location on the triangle
+  moab::GeomUtil::closest_location_on_tri(location, coords.array(), result);
+  // return the distance to the triangle location
   return (result - location).length();
 }
